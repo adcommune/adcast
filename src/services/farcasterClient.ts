a75efab53e7de2e049ceadd416b7fe19a29ad928
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-06-10 00:28:50 +0200

    feat: fetch conversation w/ cast

--- src/services/farcasterClient.ts
+++ src/services/farcasterClient.ts
@@ -1,9 +1,16 @@
 import {
   CastWithInteractions,
+  ConversationConversation,
   User,
 } from "@neynar/nodejs-sdk/build/neynar-api/v2";
 
 export class FarcasterClientAPI {
+  async fetchCast(
+    hash: string
+  ): Promise<{ conversation: ConversationConversation }> {
+    return fetch(`/api/cast/${hash}`).then((res) => res.json());
+  }
+
   async fetchFeed(): Promise<{ casts: CastWithInteractions[] }> {
     return fetch("/api/feed").then((res) => res.json());
   }
